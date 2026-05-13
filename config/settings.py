901f98074f76f443cbba833980a685828a65dc73
Rodrigo Herrainz <rherrainz@gmail.com>
2026-05-13 11:15:48 -0300

    fixed settyngs.py

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,11 @@ from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+if importlib.util.find_spec('dotenv'):
+    from dotenv import load_dotenv
+
+    load_dotenv(BASE_DIR / '.env')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
